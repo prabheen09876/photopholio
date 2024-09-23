3954c975e9eb191a8128b07a2acaa98e0936a73e
Abhii <prabheen09876@gmail.com>
2024-09-23 08:17:27 +0000

    add one more page

--- script.js
+++ script.js
@@ -60,3 +60,21 @@ document.querySelectorAll('.elem').forEach(elem => {
 
 
 
+
+function animateCards(cards) {
+    cards.forEach(card => {
+        gsap.to(card, {
+            scale: 0.6,
+            opacity: 0,
+            scrollTrigger: {
+                trigger: card,
+                start: 'top 15%',
+                end: 'bottom 15%',
+                scrub: true,
+            },
+        });
+    });
+}
+
+const cards = ['.card1', '.card2', '.card3', '.card4', '.card5', '.card6', '.card7', '.card8'];
+animateCards(cards);
